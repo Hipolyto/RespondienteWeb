5a0e5f7860bbb77533118775be2b8fd35709aa31
Hipolyto <polo@letsolutions.com.mx>
2018-02-21 14:21:26 -0600

    upload

---
+++ RespondienteWeb/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" Inherits="RespondienteWeb.Default" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>Default</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<asp:Button id="button1" runat="server" Text="Enviar Emegencia" OnClick="button1Clicked" />
+	</form>
+</body>
+</html>
---
+++ RespondienteWeb/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+using static System.Console;
+
+using FirebaseNet.Database;
+
+namespace RespondienteWeb
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        public void button1Clicked(object sender, EventArgs args)
+        {
+            // Instanciating with base URL  
+            FirebaseDB firebaseDB = new FirebaseDB("https://primerrespondiente-2c016.firebaseio.com/");  
+  
+            // Referring to Node with name "Teams"  
+            FirebaseDB firebaseDBTeams = firebaseDB.Node("Registro_de_Emergencia");  
+  
+            var data = @"{ 'nueva_emergencia': 1 }";
+  
+            WriteLine("POST Request");  
+            FirebaseResponse postResponse = firebaseDBTeams.Post(data);  
+            WriteLine(postResponse.Success);  
+            WriteLine();  
+
+
+  
+            WriteLine(firebaseDBTeams.ToString());  
+            ReadLine();  
+        }
+    }
+}
